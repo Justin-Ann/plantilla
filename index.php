88f67684183729ce1e0c4853e4c40831a3bcaa92
Justin-Ann <justinannh@gmail.com>
2025-03-24 14:09:38 +0800

    Updated files

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+// New index.php (converted from index.html)
+session_start();
+
+// Check if user is logged in
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
